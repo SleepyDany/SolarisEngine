00593b66e12311484321103d1a01b0c25959c6e9
ferz <fedotov_clever_91@mail.ru>
2024-03-22 01:32:58 +0300

    [SLR-2] Add SLR_ASSERT macro

--- Solaris/Source/Core/Core.h
+++ Solaris/Source/Core/Core.h
@@ -6,10 +6,16 @@
             #define SOLARIS_API __declspec(dllexport)
         #else
             #define SOLARIS_API __declspec(dllimport)
-        #endif
+        #endif // SLR_BUILD_DLL
     #else
         #define SOLARIS_API
-    #endif
+    #endif // SLR_DYNAMIC_LINK
 #else
     #error Solaris supports Windows
 #endif // SLR_PLATFORM_WINDOWS
+
+#ifdef SLR_ENABLE_ASSERT
+    #define SLR_ASSERT(value, ...) { if (!(value)) { SLR_CORE_LOG(Error, "Assert: {}", __VA_ARGS__); __debugbreak(); } }
+#else
+    #define SLR_ASSERT(value, ...)
+#endif // SLR_ENABLE_ASSERT
